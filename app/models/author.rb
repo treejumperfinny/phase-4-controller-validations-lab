efcf4d86d9462bf60736120bd6fd8c8022004c64
Aya Ramirez <ayaramz@gmail.com>
2022-06-30 11:08:12 -0400

    commit tree d0953253729261536c4dec4c76dfeb44ff90e996

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,9 @@
 class Author < ApplicationRecord
+    has_many :posts
+
+    validates :name, presence: true
+    validates :content, length: {maximum: 100}
+
+  
 end
+
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+    belongs_to :author
+
+    
+    validates :title, presence: true
+    validates :content, length: {maximum: 100}
+    
 end
